146e7468e2ba4b02600a9d3f5f56fb2a9fa96f12
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-11-05 03:28:06 +0530

    Add files via upload

---
+++ Tool.java
@@ -0,0 +1,27 @@
+package version_e.q2;
+
+public class Tool {
+	private String productName;
+	private Integer quantity;
+	private Double unitPrice;
+	
+	public Tool() {}
+
+	public Tool(String productName, Integer quantity, Double unitPrice) {
+		this.productName = productName;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+}
